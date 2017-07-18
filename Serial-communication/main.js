05fbf54d40c739e7366c44ca190bf64b2f023154
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-18 10:36:29 +0200

    Added events to serial communication

--- Serial-communication/main.js
+++ Serial-communication/main.js
@@ -1,58 +1,86 @@
-var SerialPort = require('serialport');
-var globals = require('./globals');
+var helpers = require('./SerialHelpers');
 
-var devices_to_register = 1; //this should be given from the user (1 - 255)
+var devicesToRegister = 1; //this should be given from the user (1 - 255)
 var handshakeSucceded = false;
-var id_acceptation_stream_active = false;
+var isAcceptationIDStreamActive = false;
 var accepted_ids = 0;
 
-var port = new SerialPort('COM7',{
-  baudRate: 9600
-}, onPortOpened);
+helpers.init('COM7');
+helpers.handshakeHandler = handleHandshake;
+helpers.idCheckRequestHandler = handleIDCheckRequest;
+helpers.idStreamStartHandler = handleIDStreamStartMessage;
+helpers.idStreamValueHandler = handleIDStreamValueMessage;
+helpers.idStreamEndHandler = handleIDStreamEndMessage;
 
-port.on('data',(data) =>{
-  console.log('Received: ' + data);
+function handleHandshake(){
   if(!handshakeSucceded){
-    handshakeSucceded = assertHandshake(data);
-    if(handshakeSucceded){
-        port.write(globals.DEVICES_NO_PACKET);
-        port.write(devices_to_connect);
-    }
-  } else if(globals.isIDCheckRequest(data)){
-    var _id = globals.read32bitInt(data,1);
-    //query mongodb here
-    //"SELECT ID FROM devices WHERE ID = \"" + _id + ""\""
-    return 0;//Number of lines found in mongo
-  }else if (!id_acceptation_stream_active && globals.isIDStreamStartPacket(data)) {
-    id_acceptation_stream_active = true;
-  } else if (id_acceptation_stream_active && globals.isIDStreamEndPacket(data)) {
-    !id_acceptation_stream_active = false;
-    if(accepted_ids == devices_to_register)
-        //operation completed, go to device association phase
-    else {
-      //Operation failed, abort
-    }
-  } else if (id_acceptation_stream_active && globals.isIDStreamValuePacket(data)) {
-    var _id = globals.read32bitInt(data,1);
-    var type = data[5];
-    //write data to database
-    //LoRa/devices "{\"ID\" : \"" + _id + "\", \"type\" : \"" + type + "\"}"
-    accepted_ids++;
+    handshakeSucceded = true;
+    helpers.answerToHandshake();
+    helpers.sendDevicesNumberPacket(devicesToRegister);
+    return;
   }
-});
-
-function onPortOpened(err){
-  if(err != null){
-      return console.log("Serial port error: ",err.message);
-  }
-  console.log("Port " + this.path + " opened succesfully");
+  console.log("Warning: unexpected handshake attempt");
 }
 
-function assertHandshake(data){
-  if(data == "H"){
-    port.write("W");
-    return true;
+function handleIDCheckRequest(id){
+  if(!checkHendshakeState())
+      return;
+  if(isAcceptationIDStreamActive){
+    console.log("Warning: received ID check request while id submission stream is open [All IDs were already checked]");
+    return;
   }
-  console.log("Message received was not expected");
-  return false;
+  console.log("Received ID to check: " + id);
+  //query mongodb here
+  //"SELECT ID FROM devices WHERE ID = \"" + _id + ""\""
+  return 0;//Number of lines found in mongo*/
+}
+
+function handleIDStreamStartMessage(){
+  if(!checkHendshakeState())
+      return;
+  if(isAcceptationIDStreamActive){
+    console.log("Warning: received id stream start packet while stream is already open");
+    return;
+  }
+  isAcceptationIDStreamActive = true;
+  console.log("ID stream started");
+}
+
+function handleIDStreamValueMessage(id,type){
+  if(!checkHendshakeState())
+      return;
+  if(!isAcceptationIDStreamActive){
+    console.log("Warning: received an id stream packet while stream is closed");
+    return;
+  }
+  console.log("\t Device-> ID: " + id + " type: " + type);
+  //write data to database
+  //LoRa/devices "{\"ID\" : \"" + _id + "\", \"type\" : \"" + type + "\"}"
+  accepted_ids++;
+}
+
+function handleIDStreamEndMessage(){
+  if(!checkHendshakeState())
+      return;
+  if(!isAcceptationIDStreamActive){
+    console.log("Warning: received id stream end message while stream is already closed");
+    return;
+  }
+  isAcceptationIDStreamActive = false;
+  console.log("ID stream ended");
+  if(accepted_ids == devices_to_register)
+      //operation completed, go to device association phase
+      console.log("operation completed");
+  else {
+    console.log("Operation failed");
+    //Operation failed, abort
+  }
+}
+
+function checkHendshakeState(){
+  if(!handshakeSucceded){
+    console.log("Warning: received a serial message without completing the handshake");
+    return false;
+  }
+  return true;
 }
