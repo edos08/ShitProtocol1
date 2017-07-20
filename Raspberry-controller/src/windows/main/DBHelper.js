cabb9aaddc0a460ccea8a67a457a06c0b4ea2698
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-20 17:21:57 +0200

    added equal

--- Raspberry-controller/src/windows/main/DBHelper.js
+++ Raspberry-controller/src/windows/main/DBHelper.js
@@ -31,7 +31,7 @@ function fillContentDivWithDevices(container,roomID){
   knex.withSchema('LoRa')
   .select('Devices.ID','Devices.Description','Device_types.Description')
   .from('Devices')
-  .where('Room',roomID)
+  .where('Room','=',roomID)
   .innerJoin('Devices.Type','Device_types.ID')
   .then(function(devices){
     var content = "<ul>";
