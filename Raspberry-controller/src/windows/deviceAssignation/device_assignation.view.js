c64122eeec1af3a544284dd0ece1a3dd96458061
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-21 15:53:56 +0200

    test unassigned devices

--- Raspberry-controller/src/windows/deviceAssignation/device_assignation.view.js
+++ Raspberry-controller/src/windows/deviceAssignation/device_assignation.view.js
@@ -1,3 +1,15 @@
+var dbHelper = require('../main/DBHelper');
+var Dialogs = require('dialogs');
+var dialogs = Dialogs();
+
 function setupComponents(){
-  
+  dbHelper.queryAllDevicesWithNoRoomAssignedAndShowIn(document.getElementById('devicesContainer'));
+}
+
+onDeviceRenameButtonClick(id){
+  dialogs.prompt("Inserisci il nuovo nome per il dispositivo: ",function(name){
+    if(name != null && name != undefined && name != "" && name != " "){
+      dbHelper.renameDevice(id,name);
+    }
+  });
 }
