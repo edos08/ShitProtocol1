7a9e8658353a1701bd4751cdffcba589ea957c57
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-24 10:00:36 +0200

    assigning device to room

--- Raspberry-controller/src/app.js
+++ Raspberry-controller/src/app.js
@@ -5,12 +5,12 @@ var registration = require('./registration_process');
 
 var dbHelper =require('./windows/main/DBHelper');
 
-
-
-
 let window;
 let deviceAssignationWindow;
+let chooseRoomWindow;
+
 var registrationActive = false;
+var currentDeviceForWhichTheRoomIsBeingChosen = -1;
 
 app.on('ready', function(){
   window = new BrowserWindow({
@@ -23,8 +23,20 @@ app.on('ready', function(){
 
   window.on('closed',() =>{
     window = null;
-  })
+  });
+
+  window.on('will-quit',() =>{
+    registration.terminate();
+  });
 });
+
+function onRegistrationEnd(result){
+  console.log("Registration succesful: " + result);
+  registrationActive = false;
+  deviceAssignationWindow = new BrowserWindow({parent: window, modal: true});
+  deviceAssignationWindow.openDevTools();
+  deviceAssignationWindow.loadURL('file://' + __dirname + '/windows/deviceAssignation/device_assignation.html');
+}
 
 ipc.on("register_devices_pressed",function(){
   console.log("Congratualtions, you have pressed the register devices button");
@@ -37,15 +49,28 @@ ipc.on("insert_room_button_pressed", function(){
 });
 
 ipc.on('assign_devices_button_pressed',function(){
-  deviceAssignationWindow = new BrowserWindow({parent: window, modal: true});
+  deviceAssignationWindow = new BrowserWindow({
+    parent: window,
+    modal: true
+  });
   deviceAssignationWindow.openDevTools();
   deviceAssignationWindow.loadURL('file://' + __dirname + '/windows/deviceAssignation/device_assignation.html');
 });
 
-function onRegistrationEnd(result){
-  console.log("Registration succesful: " + result);
-  registrationActive = false;
-  deviceAssignationWindow = new BrowserWindow({parent: window, modal: true});
-  deviceAssignationWindow.openDevTools();
-  deviceAssignationWindow.loadURL('file://' + __dirname + '/windows/deviceAssignation/device_assignation.html');
+ipc.on('room_assignation_button_pressed',function(event,deviceID){
+
+  chooseRoomWindow = new BrowserWindow({
+    parent: deviceAssignationWindow,
+    modal: true,
+    width:600,
+    height: 200
+  })
+
+  var chooseRoomWindowURL = 'file://' + __dirname + '/windows/deviceAssignation/choose_room_dialog.html';
+  chooseRoomWindow.loadURL(chooseRoomWindowURL);
+  currentDeviceForWhichTheRoomIsBeingChosen = deviceID;
+})
+
+ipc.on('room_assignation_ok_button_pressed'function(event,roomID)){
+  dbHelper.assignDeviceToRoom(currentDeviceForWhichTheRoomIsBeingChosen,roomID);
 }
--- Raspberry-controller/src/windows/deviceAssignation/choose_room_dialog.view.js
+++ Raspberry-controller/src/windows/deviceAssignation/choose_room_dialog.view.js
@@ -1,4 +1,4 @@
-var dbHelper = require('../main/DBHelper');
+var ipc = require('electron').ipcRenderer;
 
 function setUpComponents(){
    fillRoomsList();
@@ -6,4 +6,13 @@ function setUpComponents(){
 
 function fillRoomsList(){
     dbHelper.fillRoomsList(document.getElementById('rooms_list'));
+}
+
+function addOkButtonClickLIstener(){
+  var okButton = document.getElementById('ok_button');
+  okButton.addEventListener('click',function(){
+    var roomList = document.getElementById('rooms_list');
+    var roomID = roomList.options[roomList.selectedIdex].value;
+    ipc.send('room_assignation_ok_button_pressed',roomID);
+  })
 }
