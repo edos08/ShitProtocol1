8fbe5be7829122dc1cb38c0af0f4c2009fe4dadf
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-25 15:36:01 +0200

    ops

--- Raspberry-controller/src/windows/sensorsAssignation/sensorsAssignation.view.js
+++ Raspberry-controller/src/windows/sensorsAssignation/sensorsAssignation.view.js
@@ -15,7 +15,6 @@ function onDeviceRenameButtonClick(button){
   dialogs.prompt("Inserisci il nuovo nome per il dispositivo: ",function(name){
     if(name != null && name != undefined && name != "" && name != " "){
       ipc.send('rename-device',deviceID,name);
-      //dbHelper.renameDevice(button.parentNode.id,name,remote.getCurrentWindow());
     }
   });
 }
