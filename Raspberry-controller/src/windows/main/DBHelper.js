7bb6b86e6c2515f79725df834277c3d8ca0f1a9e
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-26 17:16:32 +0200

    typo

--- Raspberry-controller/src/windows/main/DBHelper.js
+++ Raspberry-controller/src/windows/main/DBHelper.js
@@ -177,7 +177,7 @@ function fillRoomNameContainer(roomID,after){
 
 function getDeviceInfo(deviceID,after){
   knex.withSchema('LoRa')
-  .select('devs.ID ad id','devs.Description as description','devs.Sensor as sensorID','devs2.Description as sensor','devs.LightValue as value')
+  .select('devs.ID as id','devs.Description as description','devs.Sensor as sensorID','devs2.Description as sensor','devs.LightValue as value')
   .innerJoin('Devices as devs2','devs.Sensor','devs2.ID')
   .from('Devices as devs')
   .where('ID',deviceID)
