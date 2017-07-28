138c0d1d7b98aa11c9857d599ed709c22868c724
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-28 17:24:13 +0200

    ok

--- Raspberry-controller/src/windows/main/main.view.js
+++ Raspberry-controller/src/windows/main/main.view.js
@@ -108,7 +108,7 @@ function onDeviceClicked(device){
 
 ipc.on('device-info-gathered',(event,device) => {
   console.log("Displaying infoos");
-  content = /*"<table>"*/ ""
+  content = "<table>"
   + "<tr>"
   + "<td> Dispositivo: " + device.description + "</td> <td><button id = \"" + device.id + "\" onClick=\"onDeviceRenameButtonClick(this)\"> Rinomina </button></td></br> "
   + "</tr><tr>"
