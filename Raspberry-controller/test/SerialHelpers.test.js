e47e2971342bd7d90d0e279d78a12e1eee774eb2
Ivano Donadi <ivadonadi98@gmail.com>
2017-07-31 18:32:17 +0200

    more tests

--- Raspberry-controller/test/SerialHelpers.test.js
+++ Raspberry-controller/test/SerialHelpers.test.js
@@ -1,55 +1,6 @@
 var serialHelpers = require('../src/SerialHelpers');
-const SerialPort = require('serialport');
 
-//mock port opening
-SerialPort.prototype.open = jest.fn( (onPortOpened) => onPortOpened(null));
-
-test('should be handshake packet',() => {
-  expect(serialHelpers.isHandshakePacket('H')).toBeTruthy();
-})
-
-
-test('should be handshakeEnd packet',() => {
-  expect(serialHelpers.isHandshakeEndPacket('A')).toBeTruthy();
-})
-
-test('should be idCheckRequest packet',() => {
-  var buf = Buffer.alloc(5);
-  buf[0] = 1;
-  expect(serialHelpers.isIDCheckRequest(buf)).toBeTruthy();
-})
-
-test('should be a sendResult packet',() =>{
-  var buf = Buffer.alloc(2);
-  buf[0] = 6;
-  buf[1] = 1;
-  expect(serialHelpers.isSendResultPacket(buf)).toBeTruthy();
-})
-
-test('should be idStreamStartPacket', () => {
-  var buf = Buffer.alloc(2);
-  buf[0] = 2;
-  expect(serialHelpers.isIDStreamStartPacket(buf)).toBeTruthy();
-})
-
-test('should be idStreamEndPacket', () => {
-  var buf = Buffer.alloc(2);
-  buf[0] = 2;
-  buf[1] = 255;
-  expect(serialHelpers.isIDStreamEndPacket(buf)).toBeTruthy();
-})
-
-test('should be isIDStreamValuePacket', () => {
-  var buf = Buffer.alloc(6);
-  buf[0] = 2;
-  expect(serialHelpers.isIDStreamValuePacket(buf)).toBeTruthy();
-})
-
-test('should be registrationModeEnteredPacket',() => {
-  var buf = Buffer.alloc(1);
-  buf[0] = 3;
-  expect(serialHelpers.isRegistrationModeEnteredPacket(buf)).toBeTruthy();
-})
+const mockFunc = jest.fn(() => 1);
 
 test('should read 0xAB192837',() => {
   var buf = Buffer.alloc(4);
@@ -61,27 +12,81 @@ test('should read 0xAB192837',() => {
   expect((result >>> 0)).toBe(0xAB192837);
 })
 
-test('should call on port opened: ',() => {
-  serialHelpers.openPort();
+test('should write : 0xAB, 0x19, 0x28, 0x37',() => {
+  var buf = Buffer.alloc(4);
+  serialHelpers.write32BitInt(buf,0,0xAB192837);
+  expect(buf[0]).toBe(0xAB);
+  expect(buf[1]).toBe(0x19);
+  expect(buf[2]).toBe(0x28);
+  expect(buf[3]).toBe(0x37);
 })
 
-test('check handlers connection',() => {
+test('check handshake connection and call',() => {
   serialHelpers.connectHandlers({
-     handshakeHandler: () => {},
-     handshakeEndHandler: () => {},
-     idCheckRequestHandler: () => {},
-     idStreamStartHandler: () => {},
-     idStreamValueHandler: () => {},
-     idStreamEndHandler: () => {},
-     registrationModeEnteredHandler: () => {},
-     sendResultHandler: () => {}
+     handshakeHandler: mockFunc
   });
-  expect(serialHelpers.handshakeHandler).not.toBeUndefined();
-  expect(serialHelpers.handshakeEndHandler).not.toBeUndefined();
-  expect(serialHelpers.idCheckRequestHandler).not.toBeUndefined();
-  expect(serialHelpers.idStreamStartHandler).not.toBeUndefined();
-  expect(serialHelpers.idStreamValueHandler).not.toBeUndefined();
-  expect(serialHelpers.idStreamEndHandler).not.toBeUndefined();
-  expect(serialHelpers.registrationModeEnteredHandler).not.toBeUndefined();
-  expect(serialHelpers.sendResultHandler).not.toBeUndefined();
+  var handshakeMessage = Buffer.alloc(1,'H');
+  expect(serialHelpers.callPacketHandler(handshakeMessage)).toBe(1);
+})
+
+test('check handshakeEnd connection and call',() => {
+  serialHelpers.connectHandlers({
+     handshakeEndHandler: mockFunc
+  });
+  var handshakeEndMessage = Buffer.alloc(1,'A');
+  expect(serialHelpers.callPacketHandler(handshakeEndMessage)).toBe(1);
+})
+
+test('check idCheckRequest connection and call',() => {
+  serialHelpers.connectHandlers({
+     idCheckRequestHandler: mockFunc
+  });
+  var idCheckMessage = Buffer.alloc(5,1);
+  expect(serialHelpers.callPacketHandler(idCheckMessage)).toBe(1);
+})
+
+test('check idStreamStart connection and call',() => {
+  serialHelpers.connectHandlers({
+     idStreamStartHandler: mockFunc
+  });
+  var idStreamStartMessage = Buffer.alloc(2);
+  idStreamStartMessage[0] = 2;
+  idStreamStartMessage[1] = 0;
+  expect(serialHelpers.callPacketHandler(idStreamStartMessage)).toBe(1);
+})
+
+test('check idStreamEnd connection and call',() => {
+  serialHelpers.connectHandlers({
+     idStreamEndHandler: mockFunc
+  });
+  var idStreamEndMessage = Buffer.alloc(2);
+  idStreamEndMessage[0] = 2;
+  idStreamEndMessage[1] = 255;
+  expect(serialHelpers.callPacketHandler(idStreamEndMessage)).toBe(1);
+})
+
+test('check idStreamValue connection and call',() => {
+  serialHelpers.connectHandlers({
+     idStreamValueHandler: mockFunc
+  });
+  var idStreamValueMessage = Buffer.alloc(6);
+  idStreamValueMessage[0] = 2;
+  expect(serialHelpers.callPacketHandler(idStreamValueMessage)).toBe(1);
+})
+
+test('check registrationModeEntered connection and call',() => {
+  serialHelpers.connectHandlers({
+     registrationModeEnteredHandler: mockFunc
+  });
+  var registrationModeEnteredMessage = Buffer.alloc(1,3);
+  expect(serialHelpers.callPacketHandler(registrationModeEnteredMessage)).toBe(1);
+})
+
+test('check idStreamValue connection and call',() => {
+  serialHelpers.connectHandlers({
+     sendResultHandler: mockFunc
+  });
+  var sendResultMessage = Buffer.alloc(2);
+  sendResultMessage[0] = 6;
+  expect(serialHelpers.callPacketHandler(sendResultMessage)).toBe(1);
 })
