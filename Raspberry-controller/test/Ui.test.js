a38b5afbd8ab883607c7c996433e8cfd9f9b853f
Ivano Donadi <ivadonadi98@gmail.com>
2017-08-01 12:39:32 +0200

    get rid of it

--- Raspberry-controller/test/Ui.test.js
+++ Raspberry-controller/test/Ui.test.js
@@ -2,33 +2,26 @@ var Application = require('spectron').Application;
 var assert = require('assert');
 let electronPath = require('electron');
 const path = require('path');
-let app;
+let app = new Application({
+	path: electronPath,
+	args: [
+		path.join(__dirname,"..",'src','app')
+	]
+})
 
-describe('application launch', function () {
-  this.timeout(10000)
 
-  beforeEach(function () {
-    this.app = new Application({
-      path: electronPath,
-			args: [
-				path.join(__dirname,"..",'src','app')
-			]
-    })
-    return this.app.start()
-  })
-
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
-    }
-  })
-
-  it('opens device info', function () {
-		this.app.client.element('.left.col').click('#7').then(() => {
-			this.app.client.element('.right.col').click('#150').then(() => {
-				assert.equal(1,1);
-			})
+app.start().then(() => {
+	return app.browserWindow.isVisible();
+}).then((isVisible) => {
+	assert.equal(isVisible,true);
+}).then(() =>{
+	app.client.element('.left.col').click('#7').then(() => {
+		app.client.element('.right.col').click('#150').then(() => {
+			assert.equal(1,1);
 		})
-  })
-
+	})
+}).then(() => {
+	return app.stop();
+}).catch((error) => {
+	console.error('Test failed',error.message);
 })
