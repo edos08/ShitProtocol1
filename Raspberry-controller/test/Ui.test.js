4e1fe22f98ad778852d42e83669c3d97acd23720
Ivano Donadi <ivadonadi98@gmail.com>
2017-08-01 12:43:25 +0200

    test

--- Raspberry-controller/test/Ui.test.js
+++ Raspberry-controller/test/Ui.test.js
@@ -9,19 +9,20 @@ let app = new Application({
 	]
 })
 
-
-app.start().then(() => {
-	return app.browserWindow.isVisible();
-}).then((isVisible) => {
-	assert.equal(isVisible,true);
-}).then(() =>{
-	app.client.element('.left.col').click('#7').then(() => {
-		app.client.element('.right.col').click('#150').then(() => {
-			assert.equal(1,1);
+it('test'){
+	app.start().then(() => {
+		return app.browserWindow.isVisible();
+	}).then((isVisible) => {
+		assert.equal(isVisible,true);
+	}).then(() =>{
+		app.client.element('.left.col').click('#7').then(() => {
+			app.client.element('.right.col').click('#150').then(() => {
+				assert.equal(1,1);
+			})
 		})
+	}).then(() => {
+		return app.stop();
+	}).catch((error) => {
+		console.error('Test failed',error.message);
 	})
-}).then(() => {
-	return app.stop();
-}).catch((error) => {
-	console.error('Test failed',error.message);
-})
+}
