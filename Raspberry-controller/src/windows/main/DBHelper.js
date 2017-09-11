7085737f713ec294791453c8734cd4dc932d2508
Ivano Donadi <ivadonadi98@gmail.com>
2017-09-11 17:13:33 +0200

    typo on table name

--- Raspberry-controller/src/windows/main/DBHelper.js
+++ Raspberry-controller/src/windows/main/DBHelper.js
@@ -189,7 +189,7 @@ function getDeviceInfo(deviceID,after){
   knex.withSchema('LoRa')
   .select('devs.ID as id','devs.Type as type','devs.Description as description','devs.Sensor as sensorID','devs2.Description as sensor','devs.LightValue as value','desc.Time as time')
   .leftJoin('Devices as devs2','devs.Sensor','devs2.ID')
-  .leftJoin('Status_log as stats','devs.ID','stats.id')
+  .leftJoin('Status_log as stats','devs.ID','stats.ID')
   .from('Devices as devs')
   .where('devs.ID',deviceID)
   .orderBy('stats.Time','desc')
