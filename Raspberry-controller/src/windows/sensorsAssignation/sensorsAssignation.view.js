7e61987160c9cc5fc6fe5b8226d85742f7c153a8
Ivano Donadi <ivadonadi98@gmail.com>
2017-09-12 17:33:23 +0200

    fixed id calls

--- Raspberry-controller/src/windows/sensorsAssignation/sensorsAssignation.view.js
+++ Raspberry-controller/src/windows/sensorsAssignation/sensorsAssignation.view.js
@@ -39,11 +39,11 @@ function populateListItemWithDeviceInfo(device){
 function onDeviceRenameButtonClick(button){
   dialogs.prompt("Inserisci il nuovo nome per il dispositivo: ",function(name){
     if(name != null && name != undefined && name != "" && name != " "){
-      ipc.send('rename-device',button.parentNode.id,name);
+      ipc.send('rename-device',button.parentNode.parentNode.id,name);
     }
   });
 }
 
 function onDeviceAssignSensorButtonClick(button){
-  ipc.send('sensor_assignation_button_pressed',button.parentNode.id);
+  ipc.send('sensor_assignation_button_pressed',button.parentNode.parentNode.id);
 }
