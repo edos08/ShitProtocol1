e29a4981cfe39690cdb7311b94a32cc4d74978d1
Ivano Donadi <ivadonadi98@gmail.com>
2017-09-12 14:17:13 +0200

    missing -

--- Raspberry-controller/src/windows/main/main.view.js
+++ Raspberry-controller/src/windows/main/main.view.js
@@ -149,7 +149,7 @@ function showStatInfo(device){
   statDate.setHours(statDate.getHours() + 2); //la locale della raspberry è sballata di 2 ore
   var active = (device.statValue <= 1023);
   return (
-    "<div class =\"card card-outline" + ((active) ? "success" : "warning") +"\">"
+    "<div class =\"card card-outline-" + ((active) ? "success" : "warning") +"\">"
       + "<div class = \"card-header\">"   
         + "<h3 class = \"card-title\"> Informazioni </h3>"
       + "</div>"
