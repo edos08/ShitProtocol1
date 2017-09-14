9fbad95d1d901eb0f1c00b0628251866cb1d505d
Ivano Donadi <ivadonadi98@gmail.com>
2017-09-14 11:28:32 +0200

    typo

--- Raspberry-controller/src/windows/main/main.view.js
+++ Raspberry-controller/src/windows/main/main.view.js
@@ -243,7 +243,7 @@ ipc.on('open_sensor_modal',() => {
 })
 
 ipc.on('open_room_modal',() => {
-  $('#assigRoomModal').load('../deviceAssignation/choose_room_dialog.html',() => {
+  $('#assignRoomModal').load('../deviceAssignation/choose_room_dialog.html',() => {
     $('#assignRoomModal').modal();
     chooseRoomDialog.setUpComponents();
   });
